192434d9a100812846521eba34e82a1870892dcf
lmother <sancho860@gmail.com>
2022-07-11 23:14:26 +0300

    ex00-finish

--- day02/ex00/Program.java
+++ day02/ex00/Program.java
@@ -1,28 +1,21 @@
 package day02.ex00;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Map<String, String> map = new HashMap<>();
         StringBuilder myStr = new StringBuilder();
         String signaturesPathFile = "signatures.txt";
-        String testFile = "/";
 
         FileInputStream readSignFile = null;
         FileInputStream readInputFile = null;
-        FileOutputStream writeResult  = null;
-        String inputFile = null;
-
-        while (!inputFile.equals("42")){
-            inputFile = new Scanner(System.in).nextLine();
-        }
+        FileOutputStream writeResult = null;
+        Scanner in = new Scanner(System.in);
 
         try {
             readSignFile = new FileInputStream(signaturesPathFile);
@@ -31,7 +24,7 @@ public class Program {
                 if (c == '\n'){
                     String [] tmp = myStr.toString().split(",");
                     if (tmp.length > 1)
-                        map.put(tmp[0], tmp[1].trim());
+                        map.put(tmp[0] + "\n", tmp[1].trim());
                     myStr.delete(0, myStr.length());
                     continue;
                 }
@@ -41,39 +34,40 @@ public class Program {
             if (tmp.length > 1)
                 map.put(tmp[0], tmp[1].trim());
             myStr.delete(0, myStr.length());
-
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                System.out.printf("%s, %s bit: %d\n", entry.getKey(), entry.getValue(), entry.getValue().length());
-            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+        if (readSignFile != null)
+            readSignFile.close();
 
-        try {
-            readInputFile = new FileInputStream(inputFile);
-            for (int i = 0; i < 10; i++) {
-                if(readInputFile.available() == 0)
-                    break;
-                myStr.append(String.format("%02X ", readInputFile.read()));
-            }
-            System.out.println(myStr);
-            for (Map.Entry<String, String> entry : map.entrySet()) {
-                if (myStr.toString().startsWith(entry.getValue())) {
-                    writeResult = new FileOutputStream("result.txt", true);
-                    writeResult.write(entry.getKey().getBytes(StandardCharsets.UTF_8));
-                    System.out.println(entry.getKey());
+        String inputFileName = in.nextLine();
+
+        while (!inputFileName.equals("42")) {
+            try {
+                System.out.println("PROCESSED");
+                readInputFile = new FileInputStream(inputFileName);
+                writeResult = new FileOutputStream("result.txt", true);
+
+                for (int i = 0; i < 10; i++) {
+                    if (readInputFile.available() == 0)
+                        break;
+                    myStr.append(String.format("%02X ", readInputFile.read()));
                 }
+                for (Map.Entry<String, String> entry : map.entrySet()) {
+                    if (myStr.toString().startsWith(entry.getValue())) {
+                        writeResult.write(entry.getKey().getBytes());
+                    }
+                }
+                myStr.delete(0, myStr.length());
+            } catch (IOException ex) {
+                ex.printStackTrace();
             }
-        }catch (IOException ex){
-            ex.printStackTrace();
+            inputFileName = in.nextLine();
         }
-        try {
-            if (readSignFile != null)
-                readSignFile.close();
-            if (readInputFile != null)
-                readInputFile.close();
-        }catch (IOException ex){
-            ex.printStackTrace();
-        }
+        in.close();
+        if(writeResult != null)
+            writeResult.close();
+        if (readInputFile != null)
+            readInputFile.close();
     }
 }
